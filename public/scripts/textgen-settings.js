d185dfd1dbfd919275092d8002d33e39fe0f7154
Cohee <18619528+Cohee1207@users.noreply.github.com>
2024-06-24 19:16:20 +0300

    Support more parameters for Infermatic

--- public/scripts/textgen-settings.js
+++ public/scripts/textgen-settings.js
@@ -910,6 +910,7 @@ export function parseTextgenLogprobs(token, logprobs) {
         case VLLM:
         case APHRODITE:
         case MANCER:
+        case INFERMATICAI:
         case OOBA: {
             /** @type {Record<string, number>[]} */
             const topLogprobs = logprobs.top_logprobs;
@@ -1027,7 +1028,7 @@ export function isJsonSchemaSupported() {
 }
 
 function getLogprobsNumber() {
-    if (settings.type === VLLM) {
+    if (settings.type === VLLM || settings.type === INFERMATICAI) {
         return 5;
     }
 
@@ -1131,7 +1132,7 @@ export function getTextGenGenerationData(finalPrompt, maxTokens, isImpersonate, 
         'best_of': canMultiSwipe ? settings.n : 1,
         'ignore_eos': settings.ignore_eos_token,
         'spaces_between_special_tokens': settings.spaces_between_special_tokens,
-        'seed': settings.seed,
+        'seed': settings.seed >= 0 ? settings.seed : undefined,
     };
     const aphroditeParams = {
         'n': canMultiSwipe ? settings.n : 1,
@@ -1169,6 +1170,7 @@ export function getTextGenGenerationData(finalPrompt, maxTokens, isImpersonate, 
 
     switch (settings.type) {
         case VLLM:
+        case INFERMATICAI:
             params = Object.assign(params, vllmParams);
             break;
 
--- src/constants.js
+++ src/constants.js
@@ -225,6 +225,17 @@ const INFERMATICAI_KEYS = [
     'repetition_penalty',
     'stream',
     'stop',
+    'presence_penalty',
+    'frequency_penalty',
+    'min_p',
+    'seed',
+    'ignore_eos',
+    'n',
+    'best_of',
+    'min_tokens',
+    'spaces_between_special_tokens',
+    'skip_special_tokens',
+    'logprobs',
 ];
 
 const FEATHERLESS_KEYS = [
